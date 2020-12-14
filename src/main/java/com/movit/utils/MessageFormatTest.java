3bfac0c9de891c57d6805b38f8e6660aa82ccbca
yangguoxiong <yangguoxiong@duizhuang.com>
2020-12-14 11:55:40 +0800

    StringJoiner用法

--- src/main/java/com/movit/utils/MessageFormatTest.java
+++ src/main/java/com/movit/utils/MessageFormatTest.java
@@ -1,6 +1,9 @@
 package com.movit.utils;
 
 import java.text.MessageFormat;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringJoiner;
 
 /**
  * 测试字符串多个替换方法
@@ -12,6 +15,11 @@ public class MessageFormatTest {
     }
 
     private static void test01() {
-        System.out.println(MessageFormat.format("{1}的年龄是: {0}", 18, "mark"));
+        // 测试StringJoin方法, 使用 "," 拼接list
+        StringJoiner sj = new StringJoiner(",");
+        List<String> list = Arrays.asList("最", "多", "店", "高级");
+        list.stream().forEach(sj::add);
+        System.out.println(MessageFormat.format("{1}的年龄是: {0}, 敏感词: {2}", 18, "mark", sj));
+        // 输出: mark的年龄是: 18, 敏感词: 最,多,店,高级
     }
 }

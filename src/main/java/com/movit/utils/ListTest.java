110e3c55cf1798b00885515e767a4dade639cada
yangguoxiong <yangguoxiong@duizhuang.com>
2020-12-24 15:21:12 +0800

    更新测试

--- src/main/java/com/movit/utils/ListTest.java
+++ src/main/java/com/movit/utils/ListTest.java
@@ -1,18 +1,69 @@
 package com.movit.utils;
 
 import com.google.common.collect.Lists;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 
 public class ListTest {
 
     public static void main(String[] args) {
 	    // test03();
-        test04();
+        // test04();
+        test05();
+    }
+
+    private static void test05() {
+        List<Topic> list = new ArrayList<>();
+        Topic topic1 = new Topic(0, "mark");
+        Topic topic2 = new Topic(1, "mark2");
+        Topic topic3 = new Topic(0, "mark3");
+        /*Topic topic4 = new Topic(0, "mark4");*/
+        Topic topic5 = new Topic(2, "mark5");
+        Topic topic6 = new Topic(1, "mark6");
+        /*Topic topic7 = new Topic(0, "mark7");
+        Topic topic8 = new Topic(0, "mark8");*/
+        Topic topic9 = new Topic(4, "mark9");
+        list.add(topic1);
+        list.add(topic2);
+        list.add(topic3);
+        list.add(topic5);
+        list.add(topic6);
+        list.add(topic9);
+        List<Topic> topics = Lists.newArrayList();
+        // 升序排序
+        list.sort(Comparator.comparing(Topic::getId));
+        Iterator<Topic> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            Topic topic = iterator.next();
+            if (topics.size() == 4) {
+                break;
+            }
+            if (topic.getId() == 0) {
+                topics.add(topic);
+                iterator.remove();
+            }
+        }
+        Map<Integer, Object> map = new HashMap<>();
+        // 将热门话题中对应位置排序取代
+        for (int i = 0; i < topics.size(); i++) {
+            Topic topic = topics.get(i);
+            for (int j = 0; j < list.size(); j++) {
+                Topic sortTopic = list.get(j);
+                int sort = sortTopic.getId() - 1;
+                if (sort == i && sort < 4 && !map.containsKey(sort)) {
+                    topics.set(i, sortTopic);
+                    map.put(sort, sortTopic);
+                    continue;
+                }
+            }
+        }
+        topics = topics.stream().limit(4).collect(Collectors.toList());
+        System.out.println(topics);
     }
 
     private static void test04() {
@@ -68,5 +119,13 @@ public class ListTest {
         private PointTypeEnum(int type) {
             this.type = type;
         }
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    static class Topic {
+        private Integer id;
+        private String name;
     }
 }
